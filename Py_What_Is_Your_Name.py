331870eb72746d61b99d072227d0800a2169aa8c
ExcelZero <68054173+ZeroHero77@users.noreply.github.com>
2020-08-19 11:33:50 +0100

    Create Py_What_Is_Your_Name.py
    print your name using functions and input

---
+++ Py_What_Is_Your_Name.py
@@ -0,0 +1,12 @@
+def main():
+    print_full_name(a, b)
+
+def print_full_name(a, b):
+    a = first_name
+    b = last_name
+    print('Hello '+a+' '+b+'! You just delved into python.')
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
